18c7c969576d187a85edc6b54015f2830a3f50c0
Mariana <marianasantana1008@gmail.com>
2021-04-14 11:13:45 -0300

    Applied destructuring and property shorthand

--- app.js
+++ app.js
@@ -5,20 +5,20 @@
 
  if(!address){
    console.log('Please, provide a address')
-}else {
-     geocode(address, (error, data) => {
+}else {  
+     geocode(address, (error, {latitude, longitude, location}) => {
        if(error){
          return console.log(error)
        } 
        
-       forecast(data.latitude, data.longitude, (error, forecastData) => {
+       forecast(latitude, longitude, (error, forecastData) => {
           if(error) {
              return error
           }
-          console.log('Location', data.location)
+          console.log('Location', location)
           console.log(forecastData)
        })
     })
 
- }
+ } 
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,15 +4,15 @@ const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=02193716d5d5f75c304846a494f1ebc7&query=' + latitude + ',' + longitude + '=f'
  
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, { body }) => {
        if(error) {
             callback('Unable to connect to connect to forecast service!', undefined)
-       } else if(response.body.error){
+       } else if(body.error){
             callback('Unable to find location!!', undefined)
        } else {
           callback(undefined, {
-             temperature: response.body.current.temperature,
-             feelslike: response.body.current.feelslike
+             temperature: body.current.temperature,
+             feelslike: body.current.feelslike
         })
        }
  
